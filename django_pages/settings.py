17ef415d6422349230101564b9869d7ab7bc49ae
Daniel Petrov <thedanielpetrov@gmail.com>
2023-04-06 22:48:58 -0500

    configure heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-21-(-959z18eco_uo(=9o1fnygj7*74wa84a^0@fgntulp6p@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
